75c535b6848ca28f6b09743f240489a69d64b8a8
akky25 <ry.akita1991@gmail.com>
2022-07-18 17:32:39 +0900

    msw導入

---
+++ mocks/handlers.ts
@@ -0,0 +1,18 @@
+import { rest } from "msw";
+import prefectures from "./response/prefectures.json";
+import populationComposition from "./response/populationComposition.json";
+
+export const handlers = [
+  rest.get(
+    "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(prefectures));
+    }
+  ),
+  rest.get(
+    "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear",
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(populationComposition));
+    }
+  ),
+];
